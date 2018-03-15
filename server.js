94f3943af5a00a38fbaac085f6e8a7bc0bf542a7
9000gogeta <srini.vaas.94@gmail.com>
2018-03-15 09:42:22 -0700

    Added Heroes/Items, Parse Matches

---
+++ server.js
@@ -0,0 +1,164 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
+const async = require('async');
+
+api_key = '700FB82E6669247765CE8996C33C8E88'
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine','ejs')
+app.use(express.static('public'))
+
+var first = true;
+var db;
+
+mongoClient.connect('mongodb://srini:srini94@ds263707.mlab.com:63707/dota2', (err,database) => {
+	if (err) return console.log(err)
+	db = database.db('dota2')
+	app.listen(3000, () => {
+		console.log('Client connected')
+	})
+})
+
+var minutes = 5, the_interval = minutes * 60 * 1000;
+setInterval(() => {
+  console.log("Requesting Match data every 5 minutes");
+  request('https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001?key='+api_key,{json:true},(err,res,body)=>{
+  	if(err) return err;
+  	async.each(body['result']['matches'],(item,callback)=>{
+  		addMatches('matches',item,(callback)=>{
+  			console.log('Added : Match '+callback)
+  		})
+  	},(err)=>{
+  		if(err) return console.log(err)
+  		console.log('Updated : Match Details')
+  	})
+  })
+  // do your stuff here
+}, the_interval);
+
+function addMatches(colName,query,callback){
+	db.collection(colName).update({match_id:query['match_id']},
+		query,
+		{upsert:true},(err,result)=>{
+			if(err) return console.log(err)	
+			callback(query['match_id']);
+		})
+}
+
+function getNames(callback){
+	async.parallel([
+		(callback)=>{
+			db.collection('heroes').find({},{name:1}).toArray((err,result)=>{
+				callback(null,result)
+			})
+		},
+		(callback)=>{
+			db.collection('items').find({},{name:1}).toArray((err,result)=>{
+				callback(null,result)
+			})
+		}
+	],(err,result)=>{
+		if(err) return console.log(err)
+		callback(result)	
+	})
+}
+
+app.get('/', (req,res) => {
+	if(first){
+		first = false;
+		res.redirect('/GetHeroesItems');
+	}
+	else{
+		getNames((name)=>{
+				res.render('index.ejs',{
+					heroes:name[0],
+				})
+		})
+	}
+})
+
+var downloadImg = (uri,filename,callback)=>{
+	request(uri).pipe(fs.createWriteStream(filename)).on('close',callback);
+}
+
+app.get('/getImages', (req,res)=>{
+	getNames((name)=>{
+		async.parallel([
+			(callback)=>{
+				async.each(name[0],(item,callback)=>{
+					downloadImg('http://cdn.dota2.com/apps/dota2/images/heroes/'+item.name+'_sb.png',__dirname+'/public/images/'+item.name+'.png',callback)
+				},(err)=>{
+					if(err) console.log(err)
+					console.log('Updated : Hero images')
+					callback()
+				})
+			},
+			(callback)=>{
+				async.each(name[1],(item,callback)=>{
+					downloadImg('http://cdn.dota2.com/apps/dota2/images/items/'+item.name+'_lg.png',__dirname+'/public/images/'+item.name+'.png',callback)
+				},(err)=>{
+					if(err) console.log(err)
+					console.log('Updated : Item images')	
+					callback()
+				})
+			}
+		],(err,result)=>{
+			if(err) return console.log(err)
+			res.redirect('/')
+		});
+	})
+})
+
+app.get('/getHeroesItems', (req,res) => {
+	async.parallel([
+		(callback)=>{
+			request('https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001?key='+api_key, {json:true},(err,res,body) => {
+				if(err) return console.log(err)
+				async.each(body['result']['heroes'],(item,callback)=>{
+					var heroName = item.name.replace('npc_dota_hero_','')
+					db.collection('heroes').update(
+						{name:heroName},
+						{name:heroName,id:item.id},
+						{upsert:true},(err,result)=>{
+							if(err) return console.log(err)				
+					})
+					callback()
+				},(err)=>{
+					if(err) return console.log(err)
+					console.log('Updated : Hero names')
+					callback()
+				});
+			})
+		},
+		(callback)=>{
+			request('https://api.steampowered.com/IEconDOTA2_570/GetGameItems/v0001?key='+api_key, {json:true},(err,res,body) => {
+				if(err) return console.log(err)
+				async.each(body['result']['items'],(item,callback)=>{
+					var itemName = item.name.replace('item_','')
+					db.collection('items').update(
+						{name:itemName},
+						{name:itemName,id:item.id},
+						{upsert:true},(err,result)=>{
+							if(err) return console.log(err)	
+					})
+					callback()
+				},(err)=>{
+					if(err) return console.log(err)
+					console.log('Updated : Item names')
+					callback()
+				});
+			})
+		}
+	],(err,result)=>{
+		if(err)	return console.log(err)
+		res.redirect('/getImages')	
+	});
+})
+
+app.all('*', function(req, res) {
+  res.redirect('/');
+});
